77c23c4639e2e7ab5ed73d9965588633227e2a5a
Puranam indah <purnamaindah262@gmail.com>
2018-12-04 18:48:52 +0700

    ok

--- system/core/App.php
+++ system/core/App.php
@@ -26,10 +26,14 @@ class App {
          
     } 
 
-     function p_url(){
-         $url = rtrim($_GET['url']);
+    function p_url(){
+         $url = parse_url($_GET['url']);
+         $url = $url['path'];
+         $utl = rtrim($url, '/');
          $url = filter_var($url, FILTER_SANITIZE_URL);
          $url = explode('/', $url);
          return $url;
      }
+
+    
 }
